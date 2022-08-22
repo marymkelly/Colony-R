f51438e0f817dd9153ec8aab1b5b261bdcc3a046
Ryan <107305865+RyanYeaman@users.noreply.github.com>
2022-08-22 14:27:10 -0400

    Add files via upload

--- public/js/index.js
+++ public/js/index.js
@@ -1,12 +1,72 @@
+//functionality for the model preveiws 
 const modelContainer = document.getElementsByClassName("model-image");
 const inquiryButton = document.getElementsByClassName("inquiry-button");
 
-console.log(modelContainer, inquiryButton);
-
-for (let container of modelContainer) {
+for (const container of modelContainer) {
     container.addEventListener("mouseover", () => {
-        for (let button = 0; button < inquiryButton.length; button++) {
-            inquiryButton[button].classList.add("inquir-active");
-        };
+        const button = container.children[0];
+        button.classList.add("inquire-active");
+    });
+
+    container.addEventListener("mouseout", () => {
+        const button = container.children[0];
+        button.classList.remove("inquire-active");
     });
 };
+
+// functionailty for the the inquiry box 
+
+const inquiryBox = document.querySelector("#inquire-box");
+const exitButton = document.querySelector("#inquiry-exit-button");
+const inquiryLink = document.querySelectorAll(".inquiry-button");
+
+for (const link of inquiryLink) {
+    link.addEventListener("click", (e) => {
+        e.preventDefault();
+        inquiryBox.classList.add("inquire-box-enabled");
+    });
+};
+
+if (exitButton) {
+    exitButton.addEventListener("click", () => {
+        inquiryBox.classList.remove("inquire-box-enabled");
+    });
+};
+
+//submit data to the backend for nodemailer
+
+
+const formInfo = document.querySelector(".contact-form");
+
+let fullName = document.getElementById("name");
+let email = document.getElementById("email");
+let selectedModel = document.getElementById("model-list");
+
+formInfo.addEventListener("submit", (e) => {
+    e.preventDefault();
+    
+    let formData = {
+        fullName: fullName.value,
+        email: email.value,
+        model: selectedModel.value
+    };
+
+    let xhr = new XMLHttpRequest();
+    xhr.open("POST", "/");
+    xhr.setRequestHeader("content-type", "application/json");
+    xhr.onload = function () {
+        console.log(xhr.responseText)
+        if (xhr.responseText == "success") {
+            alert("email sent");
+            fullName.value = "";
+            email.value = "";
+        } else {
+            alert("something went wrong, try again.");
+        };
+    };
+    xhr.send(JSON.stringify(formData));
+
+});
+
+
+
--- server.js
+++ server.js
@@ -1,12 +1,44 @@
+const { text } = require('express');
 const express = require('express');
 const app = express();
-const path = require("path");
+const nodemailer = require("nodemailer");
 
 app.use(express.static('public'));
+app.use(express.json());
 
 app.get("/", (req, res) => {
     console.log("Here");
-    res.sendFile(path.join(__dirname, 'index.html'));
+    res.sendFile(__dirname + "/public/index.html")
+});
+
+app.post("/", (req, res) => {
+    console.log(req.body);
+
+    const transporter = nodemailer.createTransport({
+        service: "Outlook365",
+        auth: {
+            user: "colonyr123@outlook.com",
+            pass: "Mypassword123"
+        }
+    });
+
+    const mailOptions = {
+        from: "colonyr123@outlook.com",
+        to: req.body.email,
+        model: `${req.body.model}`,
+        text: "colony-R thanks you for reaching out to inquire about our models. One of our repersentative will be reaching out to you within an hour."
+    };
+
+    transporter.sendMail(mailOptions, (error, info) => {
+        if (error) {
+            console.log(error);
+            res.send("error");
+        } else {
+            console.log("email sent" + info.response);
+        };
+    });
+
+
 });
 
 app.listen(3000)
